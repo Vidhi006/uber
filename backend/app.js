378527a54926670d9716d388cbd0e901ceccfb53
Vidhi006 <joshividhi787@gmail.com>
2026-02-21 23:14:16 +0530

    user profile done

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
 const cors = require('cors')
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.route')
 connectToDb();
 
 app.use(express.json());
 app.use(cors())
+app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     res.send('Hello World');
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/user.controller")
 const {body} = require("express-validator");
+const authMiddleware = require("../middleware/auth.middleware")
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First name should be at least 3 character long'),
@@ -16,4 +17,6 @@ router.post('/login',[
 ],
     userController.loginUser
 )
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
 module.exports = router;
