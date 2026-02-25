b2826c6e07cf51aeb4d8783f6f08a4e70157bae4
Vidhi006 <joshividhi787@gmail.com>
2026-02-26 02:35:52 +0530

    changes

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <div className='h-screen w-full'>
+            <div>
+                <h2>Get started with Uber</h2>
+                <button>Continue</button>
+            </div>
+        </div>
+    </div>
+  )
+}
