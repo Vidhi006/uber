81ab84d67603bb743c3ea59261aa15d794407539
Vidhi006 <joshividhi787@gmail.com>
2026-02-26 20:20:05 +0530

    changes

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 
 function UserLogin() {
+  const[email, setemail] = useState(' ')
   return (
-    <div>UserLogin</div>
+    <div className='p-7 h-screen flex flex-col justify-between'>
+      <div>
+        <img className='w-16 mb-10' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <form>
+          <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+          <input
+            required
+            value={email}
+            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="email"
+            placeholder='email@example.com'
+          />
+
+          <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+          <input
+            required
+            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="password"
+            placeholder='password'
+          />
+
+          <button className='bg-[#111] text-white text-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'>Login</button>
+          <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
+        </form>
+      </div>
+      <div>
+        <button className='bg-[#18b461] text-white text-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base'>
+          Sign in as Captain
+        </button>
+      </div>
+    </div>
   )
 }
 
